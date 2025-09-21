4a79e99c234d7063856859d42dcc6cfd29489ed3
tkachuk <serhii.tkachuk@visiquate.com>
2025-09-21 13:34:08 -0500

    refactored config

--- index.js
+++ index.js
@@ -7,12 +7,40 @@ const BOMParser = require("./src/js/BOM-parser");
 /* Just run task for adding field to csv by config rules as an example
 * */
 console.log('time start: ', new Date().toString());
-//fileOp.addFieldByURL(config.addField.path.csv, config.addField.options).then(() => console.log('time stop: ', new Date().toString()));
-/*fileOp.writeFile(
-    `${config.addField.path.csv}_UPD.csv`, 
-    utils.jsonToCsv(JSON.parse(fileOp.getFile(`${config.addField.path.json}.json`)), 
-    true, ','));
-console.log('time stop: ', new Date().toString());*/
 
+const runners = {
+    addField: () => fileOp.addFieldByURL(config.addField.path.csv, config.addField.options),
+    BOMConverter: () => BOMParser.licensesList(fileOp.getFile(`${config.BOMConverter.path}.json`)),
+    convert: () => fileOp.convert(config.convert.path, config.convert.separator),
+    csvToJson: () => {
+        const csv = fileOp.getFile(config.csvToJson.path, config.csvToJson.separator);
+        const json = utils.csvToJson(csv, config.csvToJson.separator);
+        fileOp.writeFile(config.csvToJson.path.replace('.csv', '.json'), JSON.stringify(json));
+    },
+    jsonToCsv: () => {
+        const json = fileOp.getFile(config.jsonToCsv.path, config.jsonToCsv.separator);
+        const arr = JSON.parse(json);
+        const csv = utils.jsonToCsv(arr, true, config.jsonToCsv.separator);
+        fileOp.writeFile(config.jsonToCsv.path.replace('.json', '.csv'), csv);
+    },
+    getFile: () => {
+        const data = fileOp.getFile(config.getFile.path, config.getFile.encoding);
+        console.log('getFile:', data);
+    },
+    writeFile: () => {
+        fileOp.writeFile(config.writeFile.path, config.writeFile.data, config.writeFile.encoding);
+        console.log('writeFile done');
+    }
+};
 
-console.log(BOMParser.licensesList(fileOp.getFile(`${config.BOMConverter.path}.json`)));
+(async () => {
+    for (const method of config.run) {
+        if (runners[method]) {
+            const result = await runners[method]();
+            if (result) console.log(`${method} result:`, result);
+        } else {
+            console.warn(`No runner for method: ${method}`);
+        }
+    }
+    console.log('time stop: ', new Date().toString());
+})();
